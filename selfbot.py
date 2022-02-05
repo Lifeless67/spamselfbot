0a88edaae43ba289a12a892c8d4dff6848aba696
astro1212 <96666061+lifeless61@users.noreply.github.com>
2022-02-05 11:16:58 -0500

    Add files via upload

---
+++ selfbot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+prefix = ('.')
+
+client = commands.Bot(command_prefix=".", self_bot=True, help_command=None)
+token = "PASTE TOKEN HERE"
+ 
+@client.event
+async def on_ready():
+    print("Selfbot Online!")
+
+@client.command()
+async def TEST(ctx):
+    await ctx. message.delete()
+    await ctx.send("Hello!")
+
+@client.command()
+async def LOLG(ctx):
+    await ctx. message.delete()
+    await ctx.send("MEN")
+
+@client.command()
+async def pingspam(ctx):
+    await ctx. message.delete()
+    for f in range(50): await ctx.send("@everyone")
+
+@client.command()
+async def help(ctx):
+    await ctx. message.delete()
+    await ctx.send("**Liquify Selfbot** \n `1: .help (brings up this menu)` \n `2: .pingspam (spams @everyone 50 times)` \n `3: .spam [message] (spams the message 100 times)`  ")
+    
+@client.command()
+async def spam(ctx, arg1):
+    await ctx. message.delete()
+    for f in range(1): await ctx.send(arg1)
+
+client.run(token, bot=False)
